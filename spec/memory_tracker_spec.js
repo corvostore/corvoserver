0fce5766831a7de047e336ee39f5c9734e3da4ec
Preeti Viswanathan <vis.preeti@gmail.com>
2017-11-09 13:12:25 -0800

    add tests for testing list item update and delete memory usage

--- spec/memory_tracker_spec.js
+++ spec/memory_tracker_spec.js
@@ -1,6 +1,7 @@
 import Store from "../store.js";
-// import CorvoLinkedList from "../corvo_linked_list.js";
-// import CorvoNode from "../corvo_node.js";
+import CorvoLinkedList from "../corvo_linked_list.js";
+import CorvoNode from "../corvo_node.js";
+import CorvoListNode from '../data_types/corvo_list_node.js';
 import MemoryTracker from "../memory_tracker";
 
 describe("MemoryTracker", () => {
@@ -138,5 +139,25 @@ describe("MemoryTracker", () => {
     testTracker.memoryUsed = 20;
 
     expect(testTracker.maxMemoryExceeded()).toBe(false);
+  });
+
+  it("uses listItemDelete to decrement memory used", () => {
+    const testTracker = new MemoryTracker(100);
+    testTracker.memoryUsed = 80;
+    const val = 'my value';
+    const testListNode = new CorvoListNode(val);
+
+    testTracker.listItemDelete(testListNode);
+    expect(testTracker.memoryUsed).toBe(40);
+  });
+
+  it("uses listItemInsert to update memory used", () => {
+    const testTracker = new MemoryTracker(100);
+
+    const val = 'my value';
+    const testListNode = new CorvoListNode(val);
+
+    testTracker.listItemInsert(testListNode.val);
+    expect(testTracker.memoryUsed).toBe(40);
   });
 });
